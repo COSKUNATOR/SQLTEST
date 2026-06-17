c56c6ca47cfb554d1663e70920260697ff1cc1b1
Joscha Ledermann <jledermann@wbsedu.de>
2026-06-17 08:37:07 +0200

    vielleicht mit code jetzt

--- src/JavaSQLBeispiel.java
+++ src/JavaSQLBeispiel.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class JavaSQLBeispiel {
+    public static void main(String[] args) {
+        Connection connection = null;
+
+        try {
+            String connectionString = "jdbc:mysql://localhost:3306/mydatabase";
+            connection = DriverManager.getConnection(connectionString,"root", "");
+
+            System.out.println("Verbindung zur Datenbank hergestellt");
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM mytable");
+
+            while (rs.next()){
+                String s = rs.getInt("id") +
+                        " " + rs.getString("name") +
+                        " " + rs.getString("beschreibung") +
+                        " " + rs.getBigDecimal("wert");
+                System.out.println(s);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(connection != null)
+                    connection.close();
+            } catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
