1b6a6f0af40dde9e1b1d38961d636e26ebc5c92d
Joscha Ledermann <jlcoskun@googlemail.com>
2026-06-26 10:02:49 +0200

    TESTPRUEFUNG

--- TESTPRUEFUNG/src/Main.java
+++ TESTPRUEFUNG/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 //        System.out.println(Mitarbeiter.registerEmployee("Frank", "Muster", "asdad", 2020, 9));
-        System.out.println(Mitarbeiter.verifyLogin("asdad", "238349"));
+        System.out.println(Mitarbeiter.verifyLogin("asdad", "238344"));
     }
 }
